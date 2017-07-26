7f82fad71422b58d5985565b088d38ee9111ddee
yangzhanmei <15809244435@163.com>
2017-07-26 09:24:52 +0800

    add deleteUser test

---
+++ test/deleteUser-spec.js
@@ -0,0 +1,44 @@
+const connection = require('../server/dbs/connect');
+const server = require('../server');
+const request = require('supertest');
+const should = require('chai').should();
+
+describe('deleteUser', () => {
+    const deleteResult = "success";
+    let defaultUser, id;
+
+    beforeEach((done) => {
+        defaultUser = {
+            user: 'yang',
+            name: '杨战美',
+            age: 21,
+            sex: '女',
+            tel: '15809244435',
+            email: '15809244435@163.com',
+            tip: '无'
+        };
+        connection.query(`delete from users`, (err, result) => {
+            const insertQuery = `insert into users values(NULL,'${defaultUser.user}','${defaultUser.name}',${defaultUser.age},'${defaultUser.sex}','${defaultUser.tel}','${defaultUser.email}','${defaultUser.tip}')`;
+            connection.query(insertQuery, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                id = result.insertId;
+                done();
+            });
+        });
+    });
+    it('delete user success', function (done) {
+        defaultUser.id = id;
+        request(server)
+            .post('/deleteUser')
+            .send({id: id})
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                res.text.should.be.equal(deleteResult);
+                done();
+            });
+    });
+});
